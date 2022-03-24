987ca0c9c65487ca499ddf3bb593526aae9525f1
g1pd78 <g1pd78@gmail.com>
2022-03-23 23:35:42 -1000

    flash messages and delete buttons

--- app.py
+++ app.py
@@ -1,15 +1,13 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 import sys
 import requests
 from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
 db = SQLAlchemy(app)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///weather.db'
-
-
-class NonExistentСity(Exception):
-    pass
+app.config.update(SECRET_KEY=os.urandom(5))
 
 
 class City(db.Model):
@@ -27,20 +25,36 @@ def index():
     city_list = City.query.all()
     weather_info = []
     for city in city_list:
-        try:
-            w = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city.name}&appid=430243c500502c9dcb2939f552371a49").json()
-            weather_info.append(w)
-        except NonExistentСity:
-            pass
+        w = requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city.name}&appid=430243c500502c9dcb2939f552371a49").json()
+        w['id'] = city.id
+        weather_info.append(w)
+
     return render_template('index.html', weather=weather_info)
 
 
 @app.route('/add', methods=['POST', 'GET'])
 def add():
     city = request.form.get('city_name')
-    if city:
-        db.session.add(City(name=city))
+    try:
+        if City.query.filter(City.name == city).first():
+            flash("The city has already been added to the list!")
+        elif requests.get(f"https://api.openweathermap.org/data/2.5/weather?q={city}&appid=430243c500502c9dcb2939f552371a49").json()['cod'] == '404':
+            flash("The city doesn't exist!")
+        else:
+            db.session.add(City(name=city))
+            db.session.commit()
+    except Exception as error:
+        flash("The city doesn't exist!")
+    return redirect(url_for('index'))
+
+
+@app.route('/delete/<city_id>', methods=['POST', 'GET'])
+def delete(city_id):
+    try:
+        db.session.delete(City.query.filter(City.id == city_id).first())
         db.session.commit()
+    except Exception:
+        pass
     return redirect(url_for('index'))
 
 
